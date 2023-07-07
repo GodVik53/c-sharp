08ca5cc83467098c5971aed61480d7e084c768b5
GodVik53 <137499699+GodVik53@users.noreply.github.com>
2023-07-07 11:23:32 +0300

    Create MinMax.cs
    программа нахождения минимума и максимума

---
+++ MinMax.cs
@@ -0,0 +1,17 @@
+// Найти минимум и максимум
+
+Console.WriteLine("Введите первое число:") ;
+int num1 = int.Parse (Console.ReadLine() ) ;
+Console.WriteLine("Введите второе число:") ;
+int num2 = int.Parse (Console.ReadLine () ) ;
+if (num1>num2) {
+    Console.WriteLine("Максимальное число") ;
+    Console.WriteLine(num1) ;
+    Console.WriteLine("Минимальное число: ") ;
+    Console.WriteLine(num2) ; }
+    else {
+    Console.WriteLine("Максимальное число") ;
+    Console.WriteLine(num2) ;
+    Console.WriteLine("Минимальное число: ") ;
+    Console.WriteLine(num1) ; } 
+
